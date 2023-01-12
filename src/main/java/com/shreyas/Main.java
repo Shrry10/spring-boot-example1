965a9b62f08a6d48f014f5b4d97363a2da6afadc
Shreyas <paridayash520@gmail.com>
2023-01-12 11:51:40 +0530

    added the files

---
+++ src/main/java/com/shreyas/Main.java
@@ -0,0 +1,61 @@
+package com.shreyas;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@SpringBootApplication
+@RestController
+@RequestMapping("api/v1/customers")
+public class Main {
+
+    private final CustomerRepository customerRepository;
+
+    public Main(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+
+    @GetMapping
+    public List<Customer> getCustomers() {
+        return customerRepository.findAll();
+    }
+
+    record NewCustomerRequest(
+            String name,
+            String email,
+            Integer age
+    ) {
+
+    }
+
+    @PostMapping
+    public void addCustomer(@RequestBody NewCustomerRequest request) {
+        Customer customer = new Customer();
+        customer.setName(request.name());
+        customer.setEmail(request.email());
+        customer.setAge(request.age());
+        customerRepository.save(customer);
+    }
+
+    @DeleteMapping("{customer_id}")
+    public void deleteCustomer(@PathVariable("customer_id") Integer id) {
+        customerRepository.deleteById(id);
+    }
+
+    @PutMapping("{customerId}")
+    public void updateCustomer(@PathVariable("customerId") Integer id, @RequestBody NewCustomerRequest request) {
+        Customer updatecustomer = customerRepository.findById(id)
+                        .orElse(null);
+        updatecustomer.setName(request.name());
+        updatecustomer.setEmail(request.email());
+        updatecustomer.setAge(request.age());
+        customerRepository.save(updatecustomer);
+    }
+}
